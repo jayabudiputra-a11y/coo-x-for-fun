f3ff9eb530ea2dbf333be61323eed6d61e832663
Budi Putra Jaya <jayabudiputra@gmail.com>
2026-02-02 12:56:38 +0700

    uda selesai

--- client/src/components/SEO/SEOHelper.jsx
+++ client/src/components/SEO/SEOHelper.jsx
@@ -1,19 +1,28 @@
 import { Helmet } from 'react-helmet-async';
 
 const SEOHelper = ({ title, description, image, slug }) => {
-  const url = `https://coo-x-for.fun/resep/${slug || ''}`;
+  const url = `https://www.coo-x-for.fun/#/resep/${slug || ''}`;
+  
   return (
     <Helmet>
       <title>{title} | Coo-X-For.Fun</title>
       <meta name="description" content={description} />
       
+      <link rel="canonical" href={url} />
+      
       <meta property="og:site_name" content="Coo-X-For.Fun" />
       <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:type" content="article" />
+
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
       
-      <link rel="alternate" type="application/rss+xml" title="Resep Terbaru" href="https://coo-x-for-fun.vercel.app/rss/recipes.xml" />
+      <link rel="alternate" type="application/rss+xml" title="Resep Terbaru" href="https://www.coo-x-for.fun/rss/index.xml" />
     </Helmet>
   );
 };
